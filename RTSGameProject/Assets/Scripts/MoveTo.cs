928ad957ed0dfdd41fd74e4c750fc73a2d2862a9
Steitz4u <kirne.st@posteo.net>
2023-12-11 18:47:54 +0000

    Updated MoveTo

--- RTSGameProject/Assets/Scripts/MoveTo.cs
+++ RTSGameProject/Assets/Scripts/MoveTo.cs
@@ -6,16 +6,18 @@ using UnityEngine.AI;
 public class MoveTo : MonoBehaviour
 {
     public Transform goal;
+    private NavMeshAgent agent;
     public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
-        agent.destination = goal.position;
+        agent.destination = transform.position;
     }
 
     // Update is called once per frame
     void Update(){
+        agent.SetDestination(goal.position);
     }
 }
