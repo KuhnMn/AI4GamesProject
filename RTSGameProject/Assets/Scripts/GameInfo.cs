eb3c1c0c848af4a887c53404aa2fa1ec0f4df3ca
Steitz4u <kirne.st@posteo.net>
2023-12-11 18:07:35 +0000

    Start On Leaders AI + New Bean Variants for Bad Beans

--- RTSGameProject/Assets/Scripts/GameInfo.cs
+++ RTSGameProject/Assets/Scripts/GameInfo.cs
@@ -11,24 +11,14 @@ public class GameInfo : MonoBehaviour{
     public int GBPoints;
     public int BBPoints;
 
-    public int TotalGBUnitPoints;
-    public int TotalBBUnitPoints;
-    public int GBUnitPoints;
-    public int BBUnitPoints;
-
     public List<GameObject> CapturePointList = new List<GameObject>();
 
     [SerializeField] private Text GBScoreText;
     [SerializeField] private Text BBScoreText;
-    
-    [SerializeField] private Text GBUnitPointsText;
-    [SerializeField] private Text BBUnitPointsText;
 
     // Start is called before the first frame update
     void Start(){
         GBScore = BBScore = GBPoints = BBPoints = 0;
-        TotalGBUnitPoints = TotalBBUnitPoints = 10;
-        GBUnitPoints = BBUnitPoints = 0;
     }
 
     // Update is called once per frame
@@ -41,28 +31,20 @@ public class GameInfo : MonoBehaviour{
         
         if(Timer>1){
             GBPoints = BBPoints = 0;
-            GBUnitPoints = BBUnitPoints = 4;
 
             foreach(GameObject CpPoints in CapturePointList){
                 if(CpPoints.tag == "GoodBean"){
                     GBPoints++;
-                    GBUnitPoints--;
                 }
                 if(CpPoints.tag == "BadBean"){
                     BBPoints++;
-                    BBUnitPoints--;
                 }
             }
             GBScore += GBPoints;
             BBScore += BBPoints;
-            TotalGBUnitPoints += GBUnitPoints;
-            TotalBBUnitPoints += BBUnitPoints;
 
             GBScoreText.text = GBScore.ToString();
             BBScoreText.text = BBScore.ToString();
-
-            GBUnitPointsText.text = TotalGBUnitPoints.ToString();
-            BBUnitPointsText.text = TotalBBUnitPoints.ToString();
 
             Timer = 0;
         }
--- RTSGameProject/Assets/Scripts/LeaderAI.cs
+++ RTSGameProject/Assets/Scripts/LeaderAI.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LeaderAI : MonoBehaviour
-{
+public class LeaderAI : MonoBehaviour{
+    private float Timer = 0;
+    
+    public string team;
+    public int Mood;
+
+    public int Priority;
+
+    //public int HighPriority;
+    //public int LowPriority;
+
+    public List<GameObject> Army = new List<GameObject>();
+    public List<GameObject> Formations = new List<GameObject>();
+    public GameObject FormationPrefab;
+    public List<GameObject> CapturePointList = new List<GameObject>();
+    //public List<GameObject> SpawnPointList = new List<GameObject>();
+    public List<GameObject> UnitTypeList = new List<GameObject>();
+
+    public int TotalUnitPoints;
+    public int UnitPoints;
+    [SerializeField] private Text UnitPointsText;
+    [SerializeField] private Text MoodText;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        Mood = (int) Random.Range(40,60);
+        TotalUnitPoints = 10;
+        team = gameObject.tag;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Timer += Time.deltaTime;
+
+        if(Timer>1){
+            UnitPoints = 4;
+            foreach(GameObject CpPoints in CapturePointList){
+                if(CpPoints.tag == team){
+                    UnitPoints--;
+                }
+            }
+            TotalUnitPoints += UnitPoints;
+
+            UnitPointsText.text = TotalUnitPoints.ToString();
+
+            Timer = 0;
+        }
+    }
+
+    void SpawnUnit(int UnitNumber,Vector3 position){
+        GameObject Bean = Instantiate(UnitTypeList[UnitNumber], position, Quaternion.identity);
+        Army.Add(Bean);
+    }
+    
+    void SpawnFormation(Vector3 position){
+        GameObject Formation = Instantiate(FormationPrefab, position, Quaternion.identity);
+        Formations.Add(Formation);
     }
 }
