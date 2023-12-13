3a7ffd29ce0ab8d6e4ef77890162b67eb628cf9e
Steitz4u <kirne.st@posteo.net>
2023-12-13 17:45:35 +0000

    Optimised fromation spwaning function

--- RTSGameProject/Assets/Scripts/LeaderAI.cs
+++ RTSGameProject/Assets/Scripts/LeaderAI.cs
@@ -39,34 +39,12 @@ public class LeaderAI : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         Timer += Time.deltaTime;
-/*
-        //prototype spawn units
-        if(TotalUnitPoints>10){
-            GameObject Bean = SpawnUnit(2,SpawnPointList[0].transform.position);
-            AvaibleUnits.Add(Bean);
-            Bean.AddComponent<MoveTo>().goal = SpawnPointList[0].transform.position;
-            TotalUnitPoints -= 10;
-        }
-        //prototype spawn formation
-        if(AvaibleUnits.Count == 9){
-            GameObject Formation = SpawnFormation(SpawnPointList[0].transform.position);
-            Formations.Add(Formation);
-            List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);  // !!! NOT WELL IMPLEMMENTED !!!
-            foreach(GameObject unit in AvaibleUnitsCopie){
-                Destroy(unit.GetComponent<MoveTo>());
-                Formation.GetComponent<Formation>().unitList.Add(unit);
-                AvaibleUnits.Remove(unit);
-            }
-            AvaibleUnitsCopie.Clear();
-            int ranNum = Random.Range(0, 3);
-            Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position;
-            Formation.GetComponent<MoveTo>().speed = 5;
-        }*/
+
         if(TotalUnitPoints>100){
-            SendFormationToPos(CapturePointList[Random.Range(0, 3)], spawnInfantryDivision(SpawnPointList[0]));
-            SendFormationToPos(CapturePointList[Random.Range(0, 3)], spawnArcherDivision(SpawnPointList[0]));
-            SendFormationToPos(CapturePointList[Random.Range(0, 3)], spawnCavalryDivision(SpawnPointList[0]));
-            SendFormationToPos(CapturePointList[Random.Range(0, 3)], spawnMilitiaDivision(SpawnPointList[0]));
+            SendFormationToPos(SpawnInfantryDivision(SpawnPointList[0]), CapturePointList[Random.Range(0, 3)].transform.position);
+            SendFormationToPos(SpawnArcherDivision(SpawnPointList[0]), CapturePointList[Random.Range(0, 3)].transform.position);
+            SendFormationToPos(SpawnCavalryDivision(SpawnPointList[0]), CapturePointList[Random.Range(0, 3)].transform.position);
+            SendFormationToPos(SpawnMilitiaDivision(SpawnPointList[0]), CapturePointList[Random.Range(0, 3)].transform.position);
             TotalUnitPoints -= 100;
         }
 
@@ -85,124 +63,95 @@ public class LeaderAI : MonoBehaviour{
             Timer = 0;
         }
     }
-
+    /*
     private GameObject SpawnUnit(int UnitNumber,Vector3 position){
         GameObject Bean = Instantiate(UnitTypeList[UnitNumber], position, Quaternion.identity);
         return Bean;
-    }
+    }*/
     
-    private GameObject SpawnFormation(Vector3 position){
-        GameObject Formation = Instantiate(FormationPrefab, position, Quaternion.identity);
+    private GameObject SpawnFormation(GameObject location, int UnitId, int UnitNumber){
+        GameObject Formation = Instantiate(FormationPrefab, location.transform.position, Quaternion.identity);
+        for(int i = 0; i < UnitNumber; i++){
+            GameObject Bean = Instantiate(UnitTypeList[UnitId], location.transform.position, Quaternion.identity);
+            AvaibleUnits.Add(Bean);
+            Bean.AddComponent<MoveTo>().goal = location.transform.position; //MoveTo
+            Bean.GetComponent<UnitStats>().InFormation = Formation;
+        }
+        Formations.Add(Formation);
+
+        List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);
+        foreach(GameObject unit in AvaibleUnitsCopie){
+            Destroy(unit.GetComponent<MoveTo>());
+            Formation.GetComponent<Formation>().unitList.Add(unit);
+            AvaibleUnits.Remove(unit);
+        }
+        AvaibleUnitsCopie.Clear();
+
+        Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position; //goal
+        Formation.GetComponent<MoveTo>().speed = UnitTypeList[UnitId].GetComponent<UnitStats>().moveSpeed;
         return Formation;
     }
 
-    void SendFormationToPos(GameObject Destination, GameObject Formation){
-        Formation.GetComponent<MoveTo>().goal = Destination.transform.position;
+    private GameObject SpawnDuelUnitFormation(GameObject location, int Unit1Id, int Unit1Number, int Unit2Id, int Unit2Number){
+        GameObject Formation = Instantiate(FormationPrefab, location.transform.position, Quaternion.identity);
+
+        for(int i = 0; i < Unit1Number; i++){
+            GameObject Bean = Instantiate(UnitTypeList[Unit1Id], location.transform.position, Quaternion.identity);
+            AvaibleUnits.Add(Bean);
+            Bean.AddComponent<MoveTo>().goal = location.transform.position;
+            Bean.GetComponent<UnitStats>().InFormation = Formation;
+        }
+        for(int i = 0; i < Unit2Number; i++){
+            GameObject Bean = Instantiate(UnitTypeList[Unit2Id], location.transform.position, Quaternion.identity);
+            AvaibleUnits.Add(Bean);
+            Bean.AddComponent<MoveTo>().goal = location.transform.position;
+            Bean.GetComponent<UnitStats>().InFormation = Formation;
+        }
+            
+        Formations.Add(Formation);
+        List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);
+        foreach(GameObject unit in AvaibleUnitsCopie){
+            Destroy(unit.GetComponent<MoveTo>());
+            Formation.GetComponent<Formation>().unitList.Add(unit);
+            AvaibleUnits.Remove(unit);
+        }
+        AvaibleUnitsCopie.Clear();
+
+        Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position;
+        float unit1Speed = UnitTypeList[Unit2Id].GetComponent<UnitStats>().moveSpeed;
+        float unit2Speed = UnitTypeList[Unit2Id].GetComponent<UnitStats>().moveSpeed;
+        if(unit1Speed < unit2Speed){
+            Formation.GetComponent<MoveTo>().speed = unit1Speed;
+        }else{
+            Formation.GetComponent<MoveTo>().speed = unit2Speed;
+        }
+        return Formation;
+    }
+
+    void SendFormationToPos(GameObject Formation, Vector3 Destination){
+        if(Formation.GetComponent<MoveTo>().goal == null){
+            Formation.AddComponent<MoveTo>().goal = Destination;
+        }
+        Formation.GetComponent<MoveTo>().goal = Destination;
+    }
+
+    private GameObject SpawnInfantryDivision(GameObject location){
+        return SpawnFormation(location,0,12);
+    }
+
+    private GameObject SpawnArcherDivision(GameObject location){
+        return SpawnFormation(location,1,9);
+    }
+
+    private GameObject SpawnCavalryDivision(GameObject location){
+        return SpawnFormation(location,2,9);
+    }
+
+    private GameObject SpawnMilitiaDivision(GameObject location){
+        return SpawnDuelUnitFormation(location,0,8,1,4);
     }
 
     void lostUnit(){
 
-    }
-
-    /*void LostFormation(GameObject Formation){
-        Formations.Remove(Formation);
-        Destroy(Formation);
-    }*/
-
-    private GameObject spawnInfantryDivision(GameObject location){
-        GameObject Formation = SpawnFormation(location.transform.position);
-        for(int i = 0; i < 12; i++){
-            GameObject Bean = SpawnUnit(0,location.transform.position);
-            AvaibleUnits.Add(Bean);
-            Bean.AddComponent<MoveTo>().goal = location.transform.position;
-            Bean.GetComponent<UnitStats>().InFormation = Formation;
-        }
-        Formations.Add(Formation);
-        List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);
-        foreach(GameObject unit in AvaibleUnitsCopie){
-            Destroy(unit.GetComponent<MoveTo>());
-            Formation.GetComponent<Formation>().unitList.Add(unit);
-            AvaibleUnits.Remove(unit);
-        }
-        AvaibleUnitsCopie.Clear();
-
-        Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position;
-        Formation.GetComponent<MoveTo>().speed = 2;
-        return Formation;
-    }
-
-    private GameObject spawnArcherDivision(GameObject location){
-        GameObject Formation = SpawnFormation(location.transform.position);
-        for(int i = 0; i < 9; i++){
-            GameObject Bean = SpawnUnit(1,location.transform.position);
-            AvaibleUnits.Add(Bean);
-            Bean.AddComponent<MoveTo>().goal = location.transform.position;
-            Bean.GetComponent<UnitStats>().InFormation = Formation;
-        }
-            
-        Formations.Add(Formation);
-        List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);
-        foreach(GameObject unit in AvaibleUnitsCopie){
-            Destroy(unit.GetComponent<MoveTo>());
-            Formation.GetComponent<Formation>().unitList.Add(unit);
-            AvaibleUnits.Remove(unit);
-        }
-        AvaibleUnitsCopie.Clear();
-
-        Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position;
-        Formation.GetComponent<MoveTo>().speed = 1;
-        return Formation;
-    }
-
-    private GameObject spawnCavalryDivision(GameObject location){
-        GameObject Formation = SpawnFormation(location.transform.position);
-        for(int i = 0; i < 9; i++){
-            GameObject Bean = SpawnUnit(2,location.transform.position);
-            AvaibleUnits.Add(Bean);
-            Bean.AddComponent<MoveTo>().goal = location.transform.position;
-            Bean.GetComponent<UnitStats>().InFormation = Formation;
-        }
-            
-        Formations.Add(Formation);
-        List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);
-        foreach(GameObject unit in AvaibleUnitsCopie){
-            Destroy(unit.GetComponent<MoveTo>());
-            Formation.GetComponent<Formation>().unitList.Add(unit);
-            AvaibleUnits.Remove(unit);
-        }
-        AvaibleUnitsCopie.Clear();
-
-        Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position;
-        Formation.GetComponent<MoveTo>().speed = 4;
-        return Formation;
-    }
-
-    private GameObject spawnMilitiaDivision(GameObject location){
-        GameObject Formation = SpawnFormation(location.transform.position);
-        for(int i = 0; i < 8; i++){
-            GameObject Bean = SpawnUnit(0,location.transform.position);
-            AvaibleUnits.Add(Bean);
-            Bean.AddComponent<MoveTo>().goal = location.transform.position;
-            Bean.GetComponent<UnitStats>().InFormation = Formation;
-        }
-        for(int i = 0; i < 4; i++){
-            GameObject Bean = SpawnUnit(1,location.transform.position);
-            AvaibleUnits.Add(Bean);
-            Bean.AddComponent<MoveTo>().goal = location.transform.position;
-            Bean.GetComponent<UnitStats>().InFormation = Formation;
-        }
-            
-        Formations.Add(Formation);
-        List<GameObject> AvaibleUnitsCopie = new List<GameObject>(AvaibleUnits);
-        foreach(GameObject unit in AvaibleUnitsCopie){
-            Destroy(unit.GetComponent<MoveTo>());
-            Formation.GetComponent<Formation>().unitList.Add(unit);
-            AvaibleUnits.Remove(unit);
-        }
-        AvaibleUnitsCopie.Clear();
-
-        Formation.AddComponent<MoveTo>().goal = CapturePointList[Random.Range(0, 3)].transform.position;
-        Formation.GetComponent<MoveTo>().speed = 0.5f;
-        return Formation;
     }
 }
